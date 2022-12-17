ae64e3d447ecfc319d5f50f88e2bbc8c0006f2cc
Ayesha Shafqat <ayeshafqat36@gmail>
2022-12-17 21:10:52 +0500

    pd2

---
+++ goat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main()
+
+{ 
+cout << " ----------------------------------       " << endl;
+cout << "                                          " << endl;
+cout << "          o    ^___^                      " << endl;
+cout << "            o  (o o)\\__________          " << endl;               
+cout << "                                          " << endl;
+cout << "               (___)\\          ) \\//\\//" << endl;
+cout << "                     ||------w  |         " << endl;
+cout << "                     ||        ||         " << endl;
+
+}
